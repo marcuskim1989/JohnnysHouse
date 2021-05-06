5305b624f860358903ee1e5a7f19ea03b1d51e7e
Marcus Kim <marcuskim1989@gmail.com>
2021-05-05 20:53:37 -0700

    Initial Commit

---
+++ Johnny's House/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Johnny's House
+//
+//  Created by Marcus Y. Kim on 5/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
